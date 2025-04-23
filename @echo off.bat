d6301d71847d6413d5d320e1ac8393316db3583e
Marco-ma-CSM <marco.ma@csm.com.hk>
2025-04-23 18:38:38 +0800

    Daily auto commit 23/04/2025 週三 18:38:38.47

--- @echo off.bat
+++ @echo off.bat
@@ -1,27 +1,23 @@
 @echo off
 cd C:\Users\Marco.ma\Documents\Embarcadero\Studio\Case1
-git init
-
-REM 檢查網路連接
-ping -n 1 github.com >nul
-if %errorlevel% neq 0 (
-    echo Network error: Cannot connect to remote repository >> git_auto_log.txt
-    exit /b 1
-)
 
 REM 記錄開始時間
 echo Starting auto git push at %date% %time% >> git_auto_log.txt
 
-REM 檢查目錄是否存在
-if not exist .git (
-    echo Error: Not a git repository >> git_auto_log.txt
-    exit /b 1
-)
+REM 檢查並移除已存在的遠端倉庫設定
+git remote remove origin
 
-git add .
-git commit -m "Initial commit"
+REM 重新添加遠端倉庫
 git remote add origin https://github.com/Marco-ma-CSM/Marco-ma-CSM-CSM-Test.git
-git push -u origin master
+
+REM 添加所有變更
+git add .
+
+REM 提交變更
+git commit -m "Daily auto commit %date% %time%"
+
+REM 強制推送到 master 分支
+git push -f origin master
 
 REM 記錄完成狀態
 if %errorlevel% equ 0 (
